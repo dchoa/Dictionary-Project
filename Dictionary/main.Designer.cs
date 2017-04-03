3662ecbf25510a5a75b95184637025b4b4910c12
unknown722437 <hoadc95@gmail.com>
2017-04-03 22:09:35 +0700

    first commit

---
+++ Dictionary/main.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace Dictionary
+{
+    partial class fDictionary
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dictionaryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.recentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.favouriteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newWordToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.onlineSearchToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pronounToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gUIEVToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tbSearch = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btSearch = new System.Windows.Forms.Button();
+            this.btOnlineSearch = new System.Windows.Forms.Button();
+            this.lbEngWord = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbMeaning = new System.Windows.Forms.Label();
+            this.lbFamily = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dictionaryToolStripMenuItem,
+            this.toolsToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1038, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dictionaryToolStripMenuItem
+            // 
+            this.dictionaryToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.recentToolStripMenuItem,
+            this.favouriteToolStripMenuItem,
+            this.quitToolStripMenuItem});
+            this.dictionaryToolStripMenuItem.Name = "dictionaryToolStripMenuItem";
+            this.dictionaryToolStripMenuItem.Size = new System.Drawing.Size(89, 24);
+            this.dictionaryToolStripMenuItem.Text = "Dictionary";
+            // 
+            // recentToolStripMenuItem
+            // 
+            this.recentToolStripMenuItem.Name = "recentToolStripMenuItem";
+            this.recentToolStripMenuItem.Size = new System.Drawing.Size(144, 26);
+            this.recentToolStripMenuItem.Text = "Recent";
+            // 
+            // favouriteToolStripMenuItem
+            // 
+            this.favouriteToolStripMenuItem.Name = "favouriteToolStripMenuItem";
+            this.favouriteToolStripMenuItem.Size = new System.Drawing.Size(144, 26);
+            this.favouriteToolStripMenuItem.Text = "Favourite";
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(144, 26);
+            this.quitToolStripMenuItem.Text = "Quit";
+            // 
+            // toolsToolStripMenuItem
+            // 
+            this.toolsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newWordToolStripMenuItem,
+            this.onlineSearchToolStripMenuItem,
+            this.pronounToolStripMenuItem});
+            this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+            this.toolsToolStripMenuItem.Size = new System.Drawing.Size(56, 24);
+            this.toolsToolStripMenuItem.Text = "Tools";
+            // 
+            // newWordToolStripMenuItem
+            // 
+            this.newWordToolStripMenuItem.Name = "newWordToolStripMenuItem";
+            this.newWordToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.newWordToolStripMenuItem.Text = "New Word";
+            // 
+            // onlineSearchToolStripMenuItem
+            // 
+            this.onlineSearchToolStripMenuItem.Name = "onlineSearchToolStripMenuItem";
+            this.onlineSearchToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.onlineSearchToolStripMenuItem.Text = "Online Search";
+            // 
+            // pronounToolStripMenuItem
+            // 
+            this.pronounToolStripMenuItem.Name = "pronounToolStripMenuItem";
+            this.pronounToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.pronounToolStripMenuItem.Text = "Pronounce";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.gUIEVToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // gUIEVToolStripMenuItem
+            // 
+            this.gUIEVToolStripMenuItem.Name = "gUIEVToolStripMenuItem";
+            this.gUIEVToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.gUIEVToolStripMenuItem.Text = "GUI E_V";
+            // 
+            // tbSearch
+            // 
+            this.tbSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbSearch.Location = new System.Drawing.Point(48, 58);
+            this.tbSearch.Name = "tbSearch";
+            this.tbSearch.Size = new System.Drawing.Size(177, 30);
+            this.tbSearch.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(48, 105);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(315, 405);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.lbFamily);
+            this.panel2.Controls.Add(this.lbMeaning);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.lbEngWord);
+            this.panel2.Location = new System.Drawing.Point(420, 105);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(546, 405);
+            this.panel2.TabIndex = 3;
+            // 
+            // btSearch
+            // 
+            this.btSearch.Location = new System.Drawing.Point(231, 58);
+            this.btSearch.Name = "btSearch";
+            this.btSearch.Size = new System.Drawing.Size(70, 31);
+            this.btSearch.TabIndex = 4;
+            this.btSearch.Text = "Search";
+            this.btSearch.UseVisualStyleBackColor = true;
+            // 
+            // btOnlineSearch
+            // 
+            this.btOnlineSearch.Location = new System.Drawing.Point(420, 58);
+            this.btOnlineSearch.Name = "btOnlineSearch";
+            this.btOnlineSearch.Size = new System.Drawing.Size(114, 31);
+            this.btOnlineSearch.TabIndex = 5;
+            this.btOnlineSearch.Text = "Online Search";
+            this.btOnlineSearch.UseVisualStyleBackColor = true;
+            // 
+            // lbEngWord
+            // 
+            this.lbEngWord.AutoSize = true;
+            this.lbEngWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbEngWord.Location = new System.Drawing.Point(60, 14);
+            this.lbEngWord.Name = "lbEngWord";
+            this.lbEngWord.Size = new System.Drawing.Size(94, 25);
+            this.lbEngWord.TabIndex = 0;
+            this.lbEngWord.Text = "Eng word";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(60, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Example:";
+            // 
+            // lbMeaning
+            // 
+            this.lbMeaning.AutoSize = true;
+            this.lbMeaning.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMeaning.Location = new System.Drawing.Point(60, 66);
+            this.lbMeaning.Name = "lbMeaning";
+            this.lbMeaning.Size = new System.Drawing.Size(88, 25);
+            this.lbMeaning.TabIndex = 3;
+            this.lbMeaning.Text = "Meaning";
+            // 
+            // lbFamily
+            // 
+            this.lbFamily.AutoSize = true;
+            this.lbFamily.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbFamily.Location = new System.Drawing.Point(60, 172);
+            this.lbFamily.Name = "lbFamily";
+            this.lbFamily.Size = new System.Drawing.Size(116, 25);
+            this.lbFamily.TabIndex = 4;
+            this.lbFamily.Text = "Family word";
+            // 
+            // fDictionary
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1038, 559);
+            this.Controls.Add(this.btOnlineSearch);
+            this.Controls.Add(this.btSearch);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.tbSearch);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "fDictionary";
+            this.Text = "Dictionary";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dictionaryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem recentToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem favouriteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newWordToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem onlineSearchToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pronounToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gUIEVToolStripMenuItem;
+        private System.Windows.Forms.TextBox tbSearch;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btSearch;
+        private System.Windows.Forms.Button btOnlineSearch;
+        private System.Windows.Forms.Label lbFamily;
+        private System.Windows.Forms.Label lbMeaning;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbEngWord;
+    }
+}
+
